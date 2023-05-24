7a943dd6581666f1962d57c0e8ab893ca409e215
Anna Wiley <awiley33@gmail.com>
2023-05-24 10:13:30 -0500

    test: Test that an object exists and has readable attributes

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,12 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+  it "exists and has readable attributes" do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student).to be_a Student
+    expect(student.name).to eq "Morgan"
+    expect(student.age).to eq 21
+    expect(student.scores).to eq []
+  end
 end
