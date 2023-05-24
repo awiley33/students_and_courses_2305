76f3eb1ac584199bbdf8ec01b8d6a876db5f8690
Anna Wiley <awiley33@gmail.com>
2023-05-24 10:32:04 -0500

    test: Test that the Course class exists and has readable attributes

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,15 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do 
+  it "exists and has readable attributes" do
+    course = Course.new("Calculus", 2)  
 
+    expect(course.name).to eq "Calculus"
+    expect(course.capacity).to eq 2
+    expect(course.students).to eq []
+  end
+
+  # # it "can check if the course is full" do
+
+  # end
 end
