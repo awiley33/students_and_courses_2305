a03f448c67f27cf8c5052f65f22b0a2868f672a0
Anna Wiley <awiley33@gmail.com>
2023-05-24 10:17:52 -0500

    test: Test that a student can log scores and earn a grade

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Student do
     expect(student.age).to eq 21
     expect(student.scores).to eq []
   end
+
+  it "can log scores and earn a grade" do
+    student = Student.new({name: "Morgan", age: 21})
+    student.log_score(89)
+    student.log_score(78)
+
+    expect(student.scores).to eq [89, 78]
+    expect(student.grade).to eq 83.5
+  end
 end
