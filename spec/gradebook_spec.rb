29363cf44347c47e31b5a7329497aacb10484c85
Anna Wiley <awiley33@gmail.com>
2023-05-24 10:57:30 -0500

    test: Add before block to test suite

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -4,10 +4,20 @@ require './lib/student'
 require './gradebook'
 
 RSpec.describe Gradebook do
+  before do
+    @gradebook = Gradebook.new("Ms. Wiley")
+    @course1 = Course.new("Calculus", 2)  
+    @course2 = Course.new("Statistics", 3)
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29})    
+    @student3 = Student.new({name: "Luca", age: 24})    
+  end
   it "exists and has readable attributes" do
-    gradebook = Gradebook.new("Ms. Wiley")
-
     expect(gradebook).to be_a Gradebook
+  end
+
+  it "starts without courses and can add them" do
+
   end
 
 end
