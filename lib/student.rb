e59906bf479da4f485088a424aafd2a8ad48f62f
Anna Wiley <awiley33@gmail.com>
2023-05-24 10:24:04 -0500

    feat: Define log score and grade methods

--- lib/student.rb
+++ lib/student.rb
@@ -8,4 +8,12 @@ class Student
     @age = student_details[:age]
     @scores = []
   end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    @scores.sum.to_f / @scores.count.to_f
+  end
 end
